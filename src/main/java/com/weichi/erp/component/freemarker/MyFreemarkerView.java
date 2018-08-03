1aae189cb3073ab02250efbf8574ccf6619a8bc0
386959653 <386959653@qq.com>
2018-08-03 17:23:45 +0800

    1、实现联系方式从数据库读取 2、实现产品列表从数据库读取 3、实现产品详情页从数据库读取 4、企业名字统一放在freemarker共享变量里

---
+++ src/main/java/com/weichi/erp/component/freemarker/MyFreemarkerView.java
@@ -0,0 +1,19 @@
+package com.weichi.erp.component.freemarker;
+
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * 自定义Freemarker共享变量
+ * Created by Wewon on 2018/8/1.
+ */
+public class MyFreemarkerView extends FreeMarkerView {
+    @Override
+    protected void exposeHelpers(Map<String, Object> model, HttpServletRequest request) throws Exception {
+        model.put("companyName", "杭州伟驰科技有限公司");
+        super.exposeHelpers(model, request);
+    }
+
+}
